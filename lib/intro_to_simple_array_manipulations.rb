15ce8280b1e34c52f28b1367e745f2b11b568904
megan <meggsnbacon777@gmail.com>
2020-07-04 06:05:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
-def using_concat()
-  array1 = ["raindrops on roses", "whiskers on kittens"]
- array1.concat ( ["sports cars", "flatiron school"])
+def using_concat(array1,array2)
+  
+ array2.concat ( [array1])
   
 end
